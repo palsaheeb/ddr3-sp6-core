bf0108383189b7715d9ef1130748612cd32f1e20
mcattin <mcattin@739e5516-d4a2-47df-ba96-5610c1fa693f>
2011-07-13 16:46:59 +0000

    Add root manifest and move SPEC specific wrapper to spec dir.
    git-svn-id: http://svn.ohwr.org/ddr3-sp6-core/trunk@49 739e5516-d4a2-47df-ba96-5610c1fa693f

--- hdl/rtl/Manifest.py
+++ hdl/rtl/Manifest.py
@@ -1,6 +1,2 @@
 files = ["ddr3_ctrl.vhd",
-         "ddr3_ctrl_wb.vhd",
-         "ddr3_ctrl_wrapper.vhd"]
-
-#modules = {"local" : "../spec/ip_cores/ddr_ctrl_bank3/user_design/rtl"}
-modules = {"svn" : "http://svn.ohwr.org/ddr3-sp6-core/trunk/hdl/spec/ip_cores/ddr_ctrl_bank3/user_design/rtl"}
+         "ddr3_ctrl_wb.vhd"]
---
+++ hdl/spec/rtl/Manifest.py
@@ -0,0 +1,3 @@
+files = ["ddr3_ctrl_wrapper.vhd"]
+
+module = {"local" : "../ip_cores/ddr_ctrl_bank3/user_design/rtl/"}
