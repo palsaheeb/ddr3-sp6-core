4b81662bdad586e7d5d5464d02542a68d91af832
mcattin <mcattin@739e5516-d4a2-47df-ba96-5610c1fa693f>
2011-07-13 13:13:09 +0000

    change manifset
    git-svn-id: http://svn.ohwr.org/ddr3-sp6-core/trunk@48 739e5516-d4a2-47df-ba96-5610c1fa693f

--- hdl/rtl/Manifest.py
+++ hdl/rtl/Manifest.py
@@ -2,4 +2,5 @@ files = ["ddr3_ctrl.vhd",
          "ddr3_ctrl_wb.vhd",
          "ddr3_ctrl_wrapper.vhd"]
 
-modules = {"local" : "../spec/ip_cores/ddr_ctrl_bank3/user_design/rtl"}
+#modules = {"local" : "../spec/ip_cores/ddr_ctrl_bank3/user_design/rtl"}
+modules = {"svn" : "http://svn.ohwr.org/ddr3-sp6-core/trunk/hdl/spec/ip_cores/ddr_ctrl_bank3/user_design/rtl"}
