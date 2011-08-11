813d8555133e006abfe6d03d4e1269d894e719b3
mcattin <mcattin@739e5516-d4a2-47df-ba96-5610c1fa693f>
2011-08-11 16:37:26 +0000

    Fix mem3_infrastructure, remove IBUFG.
    git-svn-id: http://svn.ohwr.org/ddr3-sp6-core/trunk@79 739e5516-d4a2-47df-ba96-5610c1fa693f

--- hdl/ip_cores/ddr3_ctrl_bank3_64b_32b/user_design/rtl/memc3_infrastructure.vhd
+++ hdl/ip_cores/ddr3_ctrl_bank3_64b_32b/user_design/rtl/memc3_infrastructure.vhd
@@ -164,11 +164,12 @@ begin
       --***********************************************************************
       -- SINGLE_ENDED input clock input buffers
       --***********************************************************************
-      u_ibufg_sys_clk : IBUFG
-        port map (
-          I  => sys_clk,
-          O  => sys_clk_ibufg
-          );
+      --u_ibufg_sys_clk : IBUFG
+      --  port map (
+      --    I  => sys_clk,
+      --    O  => sys_clk_ibufg
+      --    );
+    sys_clk_ibufg <= sys_clk;
   end generate;   
 
   --***************************************************************************
