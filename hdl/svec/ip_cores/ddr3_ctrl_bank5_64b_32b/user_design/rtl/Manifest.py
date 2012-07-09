12cfb76fd8e7fbc8c502abb2882344e9f01375f1
mcattin <mcattin@739e5516-d4a2-47df-ba96-5610c1fa693f>
2012-07-09 08:02:30 +0000

    Add Manifest files in generated cores directories.
    git-svn-id: http://svn.ohwr.org/ddr3-sp6-core/trunk@96 739e5516-d4a2-47df-ba96-5610c1fa693f

---
+++ hdl/svec/ip_cores/ddr3_ctrl_bank5_64b_32b/user_design/rtl/Manifest.py
@@ -0,0 +1,8 @@
+files = ["ddr3_ctrl_bank5_64b_32b.vhd",
+         "iodrp_controller.vhd",
+         "iodrp_mcb_controller.vhd",
+         "mcb_raw_wrapper.vhd",
+         "mcb_soft_calibration_top.vhd",
+         "mcb_soft_calibration.vhd",
+         "memc5_infrastructure.vhd",
+         "memc5_wrapper.vhd"]
