17dc26bbb647641d3e9deb50eded5f3984d1d9a5
mcattin <mcattin@739e5516-d4a2-47df-ba96-5610c1fa693f>
2012-07-09 14:16:43 +0000

    Add missing ddr3_ctrl_wrapper_pkg to Manifest file.
    git-svn-id: http://svn.ohwr.org/ddr3-sp6-core/trunk@97 739e5516-d4a2-47df-ba96-5610c1fa693f

--- hdl/rtl/Manifest.py
+++ hdl/rtl/Manifest.py
@@ -1,6 +1,7 @@
 files = ["ddr3_ctrl.vhd",
          "ddr3_ctrl_wb.vhd",
          "ddr3_ctrl_wrapper.vhd",
+         "ddr3_ctrl_wrapper_pkg.vhd",
          "ddr3_ctrl_pkg.vhd"]
 
 modules = {"local" : ["../spec/ip_cores/ddr3_ctrl_bank3_32b_32b/user_design/rtl",
