b8307a811f3a40c9270da9bae299b8d0126fa7ac
mcattin <mcattin@739e5516-d4a2-47df-ba96-5610c1fa693f>
2012-08-08 15:57:15 +0000

    Move common entities to a dedicated folder. Modify Manifests accordingly.
    git-svn-id: http://svn.ohwr.org/ddr3-sp6-core/trunk@107 739e5516-d4a2-47df-ba96-5610c1fa693f

---
+++ hdl/common/ip_cores/rtl/Manifest.py
@@ -0,0 +1,5 @@
+files = ["iodrp_controller.vhd",
+         "iodrp_mcb_controller.vhd",
+         "mcb_raw_wrapper.vhd",
+         "mcb_soft_calibration_top.vhd",
+         "mcb_soft_calibration.vhd"]
