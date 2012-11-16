5bd12eae28b20323f396984c5cc8c52b482444d2
Matthieu Cattin <matthieu.cattin@cern.ch>
2012-11-16 14:28:12 +0100

    Remove common files and modify Manifests to take local files.

--- hdl/common/ip_cores/rtl/Manifest.py
+++
@@ -1,5 +0,0 @@
-files = ["iodrp_controller.vhd",
-         "iodrp_mcb_controller.vhd",
-         "mcb_raw_wrapper.vhd",
-         "mcb_soft_calibration_top.vhd",
-         "mcb_soft_calibration.vhd"]
