09f14bcf12a2992507e6df054e0016c4fe76f882
Javier D. Garcia-Lasheras <javier@garcialasheras.com>
2015-06-25 16:51:22 +0200

    Fix minor issues in top Manifest.py

--- hdl/rtl/Manifest.py
+++ hdl/rtl/Manifest.py
@@ -4,7 +4,7 @@ files = ["ddr3_ctrl.vhd",
          "ddr3_ctrl_wrapper_pkg.vhd",
          "ddr3_ctrl_pkg.vhd"]
 
-local = ["../common/ip_cores/rtl"]
+local = []
 
 try:
 
@@ -13,33 +13,33 @@ try:
         if ctrl == "bank3_32b_32b":
             local.append("../spec/ip_cores/ddr3_ctrl_spec_bank3_32b_32b/user_design/rtl")
 
-        else if ctrl == "bank3_64b_32b":
+        elif ctrl == "bank3_64b_32b":
             local.append("../spec/ip_cores/ddr3_ctrl_spec_bank3_64b_32b/user_design/rtl")
 
-        else if ctrl == "bank4_32b_32b":
+        elif ctrl == "bank4_32b_32b":
             local.append("../svec/ip_cores/ddr3_ctrl_svec_bank4_32b_32b/user_design/rtl")
 
-        else if ctrl == "bank4_64b_32b":
+        elif ctrl == "bank4_64b_32b":
             local.append("../svec/ip_cores/ddr3_ctrl_svec_bank4_64b_32b/user_design/rtl")
 
-        else if ctrl == "bank5_32b_32b":
+        elif ctrl == "bank5_32b_32b":
             local.append("../svec/ip_cores/ddr3_ctrl_svec_bank5_32b_32b/user_design/rtl")
 
-        else if ctrl == "bank5_64b_32b":
+        elif ctrl == "bank5_64b_32b":
             local.append("../svec/ip_cores/ddr3_ctrl_svec_bank5_64b_32b/user_design/rtl")
 
-        else if ctrl == "bank1_32b_32b":
+        elif ctrl == "bank1_32b_32b":
             local.append("../vfc/ip_cores/ddr3_ctrl_vfc_bank1_32b_32b/user_design/rtl")
 
-        else if ctrl == "bank1_64b_32b":
+        elif ctrl == "bank1_64b_32b":
             local.append("../vfc/ip_cores/ddr3_ctrl_vfc_bank1_64b_32b/user_design/rtl")
 
         else:
-            print("[ERROR]:DDR3: The controller type is not recognnised!")
-            print("              It must be one of: bank1_32b_32b, bank1_64b_32b, bank3_32b_32b, bank3_64b_32b, bank4_32b_32b, bank4_64b_32b, bank5_32b_32b, bank5_64b_32b.")
+            raise SystemExit("[ERROR]:DDR3: The controller type is not recognised! "
+                 "It must be one of: bank1_32b_32b, bank1_64b_32b, bank3_32b_32b, bank3_64b_32b, bank4_32b_32b, bank4_64b_32b, bank5_32b_32b, bank5_64b_32b.")
 
 except NameError:
-    print("[ERROR]:DDR3: The variable ctrls MUST be defined in the top Manifest!")
+    raise SystemExit("[ERROR]:DDR3: The variable ctrls MUST be defined in the top Manifest!")
 
 
 modules = {"local" : local}
